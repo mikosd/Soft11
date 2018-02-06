2c4b08db141ffe1b9429e9cd003fb9ad7b03cc8d
Daniel Mikos <mikosd@live.com>
2018-02-06 10:01:14 -0600

    got rid of imagebuttons & tv's

--- app/src/main/java/com/example/mikos/soft11/MainActivity.java
+++ app/src/main/java/com/example/mikos/soft11/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mikos.soft11;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button_play);
+
+        button = findViewById(R.id.button_about);
+        button = findViewById(R.id.button_settings);
+
+
     }
+
+
 }

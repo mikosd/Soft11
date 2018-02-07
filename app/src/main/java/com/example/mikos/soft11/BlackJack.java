243f738dec09a56ec18a94ba9974797c23f17760
JLitman70 <johnlitman307@gmail.com>
2018-02-06 20:06:07 -0600

    Added a Deck Class that contains an ArrayList of Cards, that can be drawn from and shuffled.

--- app/src/main/java/com/example/mikos/soft11/BlackJack.java
+++ app/src/main/java/com/example/mikos/soft11/BlackJack.java
@@ -1,5 +1,8 @@
 package com.example.mikos.soft11;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by JohnL on 2/6/2018.
  */
@@ -7,11 +10,12 @@ package com.example.mikos.soft11;
 public class BlackJack{
 
     public class Card{
-        private char value;
-        private int weight;
+        private String value;
+        private int weight[];
         private char suit;
 
-        public Card(char value, char suit, int weight){
+        public Card(String value, char suit, int weight[])
+        {
             this.value = value;
             this.suit = suit;
             this.weight = weight;
@@ -19,5 +23,37 @@ public class BlackJack{
         public Card(){}
     }
 
+    public class Deck{
+        private ArrayList<Card> cards;
 
-}
+        public Deck()
+        {
+            char [] suits = {'S','H','C','D'};
+            String [] values = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+            int [][] weights = {{1,11},{2},{3},{4},{5},{6},{7},{8},{9},{10},{10},{10},{10}};
+            //cards = new Card[52];
+            int count = 0;
+            for (int i=0; i < 4;i++)
+            {
+                for (int j=0;j<13;j++) {
+                    cards.add(new Card(values[j], suits[i], weights[j]));
+                }
+        }}
+        public Card draw()
+        {
+            Card drawn = cards.get(0);
+            cards.remove(0);
+            return drawn;
+        }
+        public void shuffle()
+        {
+            Random r = new Random();
+            for (int i = cards.size()-1; i > 0; i--)
+            {
+                int j = r.nextInt(i);
+                Card temp = cards.get(i);
+                cards.set(i,cards.get(j));
+                cards.set(j,temp);
+            }
+        }
+    }}

e4e2e997eb5c0fe73754c434abde2fcb8f6c9063
JLitman70 <johnlitman307@gmail.com>
2018-02-10 16:33:55 -0600

    Created a settings page that lets you change your name and starting chips.

--- app/src/main/java/com/example/mikos/soft11/MainActivity.java
+++ app/src/main/java/com/example/mikos/soft11/MainActivity.java
@@ -5,9 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+    static final int START_SETTINGS = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +22,24 @@ public class MainActivity extends Activity {
         button_about.setOnClickListener(new aboutListener());
         button_settings.setOnClickListener(new settingsListener());
 
-    }
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == START_SETTINGS) {
+            if(resultCode == Activity.RESULT_OK){
+                String name=data.getStringExtra("name");
+                int chips = data.getIntExtra("chips",1);
+
+                Toast toast = Toast.makeText(getApplicationContext(), name+" "+chips, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+
+            }
+        }
+    }
     class playListener implements View.OnClickListener{
 
         @Override
@@ -35,7 +52,7 @@ public class MainActivity extends Activity {
 
 
 
-class aboutListener implements View.OnClickListener{
+    class aboutListener implements View.OnClickListener{
 
         @Override
         public void onClick(View view) {
@@ -53,7 +70,9 @@ class aboutListener implements View.OnClickListener{
         public void onClick(View view) {
 
             Intent settingIntent = new Intent(getApplicationContext(),SettingsActivity.class);
-            startActivity(settingIntent);
+            startActivityForResult(settingIntent,START_SETTINGS);
+
         }
     }
+
 }

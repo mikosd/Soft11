cd5cd8bbdc523ad9d903e7b5268bf577325d53e1
Spavlovich <saashaAP@yahoo.com>
2018-02-11 04:06:49 -0600

    passed the player object from main activity to play activity, switched the order of the editTexts so that name (top one) will be default.

--- app/src/main/java/com/example/mikos/soft11/PlayActivity.java
+++ app/src/main/java/com/example/mikos/soft11/PlayActivity.java
@@ -3,30 +3,44 @@ package com.example.mikos.soft11;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import static com.example.mikos.soft11.BlackJack.*;
-
 public class PlayActivity extends Activity{
-    Player player;
 
+
+    Player player;
+    Deck deck = new Deck();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
-       // BlackJack.Deck deck = new BlackJack.Deck();
+        Button button_hit = findViewById(R.id.button_hit);
+        button_hit.setOnClickListener(new hitListener());
 
-        //deck.shuffle();
+        player = (Player) getIntent().getSerializableExtra("player");
+        // Log.i("PLAYER", String.valueOf(player.chips)); tested for chip value  *working*
+        TextView chip_tv = (TextView)findViewById(R.id.tv_chips);
+        chip_tv.setText("Chips: " + player.chips);
 
-        BlackJack.Player dealer;
+        deck.shuffle(); //pre-shuffling deck, *untested*
+        Card drawn = deck.draw();
+        //Log.i("suit", drawn.getSuit());
+        //Log.i("value", drawn.getValue());
+        //Log.i("weight", String.valueOf(drawn.getWeight()));
 
-     //   int bet = findViewById(R.id.);
-
-    player = (Player) getIntent().getSerializableExtra("player");
-    // Log.i("PLAYER", String.valueOf(player.chips)); tested for chip value  *working*
     }
 
 
+    class hitListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
 
+
+
+        }
+    }
 }

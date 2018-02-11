435c15d3e5dd49f505c5dd03641879e5bce1c8d1
mikos <mikosd@live.com>
2018-02-10 23:04:23 -0600

    more stuff to playActivity

--- app/src/main/java/com/example/mikos/soft11/PlayActivity.java
+++ app/src/main/java/com/example/mikos/soft11/PlayActivity.java
@@ -3,12 +3,22 @@ package com.example.mikos.soft11;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class PlayActivity extends Activity {
+import static com.example.mikos.soft11.BlackJack.*;
+
+public class PlayActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+       // BlackJack.Deck deck = new BlackJack.Deck();
+
+        //deck.shuffle();
+
+        BlackJack.Player dealer;
+
+     //   int bet = findViewById(R.id.);
     }
 
 

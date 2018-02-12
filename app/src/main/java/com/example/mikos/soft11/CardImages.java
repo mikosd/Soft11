0b7163e9577a862bcb9a9d5e6703a684cbef1ea4
mikos <mikosd@live.com>
2018-02-12 14:52:24 -0600

    cardImage java file

---
+++ app/src/main/java/com/example/mikos/soft11/CardImages.java
@@ -0,0 +1,69 @@
+package com.example.mikos.soft11;
+
+import android.media.Image;
+
+/**
+ * Created by mikos on 2/12/2018.
+ */
+
+public class CardImages{
+/////////////////Spades////////////////
+    private final int ace_s = R.drawable.ace_s;
+    private final int king_s = R.drawable.king_s;
+    private final int queen_s = R.drawable.queen_s;
+    private final int jack_s = R.drawable.jack_s;
+    private final int ten_s = R.drawable.ten_s;
+    private final int nine_s = R.drawable.nine_s;
+    private final int eight_s = R.drawable.eight_s;
+    private final int seven_s = R.drawable.seven_s;
+    private final int six_s = R.drawable.six_s;
+    private final int five_s = R.drawable.five_s;
+    private final int four_s = R.drawable.four_s;
+    private final int three_s = R.drawable.three_s;
+    private final int two_s = R.drawable.two_s;
+
+//////////////////Hearts////////////////////////////
+    private final int ace_h = R.drawable.ace_h;
+    private final int king_h = R.drawable.king_h;
+    private final int queen_h = R.drawable.queen_h;
+    private final int jack_h = R.drawable.jack_h;
+    private final int ten_h = R.drawable.ten_h;
+    private final int nine_h = R.drawable.nine_h;
+    private final int eight_h = R.drawable.eight_h;
+    private final int seven_h = R.drawable.seven_h;
+    private final int six_h = R.drawable.six_h;
+    private final int five_h = R.drawable.five_h;
+    private final int four_h = R.drawable.four_h;
+    private final int three_h = R.drawable.three_h;
+    private final int two_h = R.drawable.two_h;
+
+/////////////////Clubs//////////////////////////////
+    private final int ace_c = R.drawable.ace_c;
+    private final int king_c = R.drawable.king_c;
+    private final int queen_c = R.drawable.queen_c;
+    private final int jack_c = R.drawable.jack_c;
+    private final int ten_c = R.drawable.ten_c;
+    private final int nine_c = R.drawable.nine_c;
+    private final int eight_c = R.drawable.eight_c;
+    private final int seven_c = R.drawable.seven_c;
+    private final int six_c = R.drawable.six_c;
+    private final int five_c = R.drawable.five_c;
+    private final int four_c = R.drawable.four_c;
+    private final int three_c = R.drawable.three_c;
+    private final int two_c = R.drawable.two_c;
+
+////////////////Diamonds///////////////////////////
+    private final int ace_d = R.drawable.ace_d;
+    private final int king_d = R.drawable.king_d;
+    private final int queen_d = R.drawable.queen_d;
+    private final int jack_d = R.drawable.jack_d;
+    private final int ten_d = R.drawable.ten_d;
+    private final int nine_d = R.drawable.nine_d;
+    private final int eight_d = R.drawable.eight_d;
+    private final int seven_d = R.drawable.seven_d;
+    private final int six_d = R.drawable.six_d;
+    private final int five_d = R.drawable.five_d;
+    private final int four_d = R.drawable.four_d;
+    private final int three_d = R.drawable.three_d;
+    private final int two_d = R.drawable.two_d;
+}
